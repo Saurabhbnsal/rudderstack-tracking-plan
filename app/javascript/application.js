3610d4506d38abe01eae97794974e714d5368eeb
saurabhbansal002 <saurabh.bansal@codingninjas.in>
2023-08-25 12:04:15 +0530

    changes in UI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,16 +3,4 @@ import "@hotwired/turbo-rails"
 // require('./packs/application')
 //= require jquery
 //= require link_to_add_fields
-
-// $("[data-form-prepend]").click(function(e) {
-//     var obj = $($(this).attr("data-form-prepend"));
-//     obj.find("input, select, textarea").each(function() {
-//         $(this).attr("name", function() {
-//             return $(this)
-//                 .attr("name")
-//                 .replace("new_record", new Date().getTime());
-//         });
-//     });
-//     obj.insertBefore(this);
-//     return false;
-// });
+//= require bootstrap-sprockets
